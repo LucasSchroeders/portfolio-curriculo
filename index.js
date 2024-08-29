c5adebdb6834a49599126a1a4960c71a5b42be41
Lucas Schroeder <lucasschroederkm@gmail.com>
2024-08-29 17:16:04 -0300

    Criacao de modal

---
+++ index.js
@@ -0,0 +1,33 @@
+window.addEventListener('scroll', function(){
+    var header = document.querySelector('nav');
+    header.classList.toggle('sticky', window.scrollY > 20)
+})
+
+function abrirModal(modalName){
+    const modal = document.getElementById(modalName)
+    const header = document.querySelector('nav')
+
+    modal.classList.add('abrir')
+    header.style.position = 'relative'
+    var fechar = 'fechar-' + modalName 
+
+    modal.addEventListener('click', (e) =>{
+        if(e.target.id == fechar || e.target.id == modalName){
+            modal.classList.remove('abrir')
+            header.style.position = 'fixed'
+        }
+    })
+}
+
+const swiper = new Swiper('.swiper', {
+    cssMode: true,
+    direction: 'horizontal',
+    loop: true,
+    pagination: {
+      el: '.swiper-pagination',
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+});
